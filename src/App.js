4ece3270dba040a8a3fe4998709193bfb72b5cc2
Itai Hammer <itaihammer1@gmail.com>
2021-07-24 12:44:34 -0700

    :rocket:

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//files
+import Main from "./pages/Main.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+//files
+import NotFound from "./NotFound.scss";
+
+export default function () {
+  return (
+    <div className="NotFound">
+      <p>Not Found</p>
+    </div>
+  );
+}
