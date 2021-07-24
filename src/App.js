6a66320955fdac8288f8fdea0858c10376343a1e
Itai Hammer <itaihammer1@gmail.com>
2021-07-24 12:59:56 -0700

    Updated ui

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //files
 import Main from "./pages/Main.jsx";
-import NotFound from "./pages/NotFound.jsx";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" component={Main} />
-        <Route component={NotFound} />
       </Switch>
     </Router>
   );
